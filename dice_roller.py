c70f534f74bb8fdf56979828f52e48a3ea64f123
Michael Thurm <leuchtthurm@gmail.com>
2021-01-30 22:10:40 +0000

    add flavour to the rolls

--- dice_roller.py
+++ dice_roller.py
@@ -2,7 +2,13 @@ import random
 
 def main():
   roll = random.randint(1,6)
-  print(f'You rolled a {roll}')
+
+  if roll == 1:
+    print(f'You rolled a {roll}! Back to the start!')
+  elif roll == 6:
+    print(f'You rolled a {roll}! Roll again!')
+  else:
+    print(f'You rolled a {roll}.')
 
 if __name__== "__main__":
   main()
